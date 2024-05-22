c151957dc1fc36109837d70630a203bf82cbc545
jhs512 <jangka512@gmail.com>
2024-05-23 01:13:34 +0900

    work

--- bin/app_1.dart
+++ bin/app_1.dart
@@ -36,6 +36,29 @@ void main() {
       todos.removeAt(index);
 
       print('$id번 할 일을 삭제했습니다.');
+    } else if (cmd == 'modify') {
+      stdout.write('수정할 할 일의 번호 : ');
+
+      final id = int.tryParse(stdin.readLineSync()!.trim()) ?? -1;
+
+      if (id == -1) {
+        print('1 이상의 숫자만 입력해 주세요.');
+        continue;
+      }
+
+      final index = todos.indexWhere((todo) => todo.id == id);
+
+      if (index == -1) {
+        print('수정할 할 일을 찾을 수 없습니다.');
+        continue;
+      }
+
+      stdout.write('내용 : ');
+      final content = stdin.readLineSync()!.trim();
+
+      todos[index].content = content;
+
+      print('$id번 할 일을 수정했습니다.');
     } else if (cmd == 'list') {
       print('번호 / 작성날짜 / 수정날짜 / 내용');
 
@@ -52,7 +75,7 @@ void main() {
 class Todo {
   static int _lastId = 0;
   final int id;
-  final String content;
+  String _content;
   final DateTime createDate;
   DateTime modifyDate;
 
@@ -60,10 +83,17 @@ class Todo {
 
   get modifyDateForPrint => modifyDate.toString().substring(2, 16);
 
-  Todo(this.content, {int? id})
+  Todo(this._content, {int? id})
       : id = id ?? _getNextId(),
         createDate = DateTime.now(),
         modifyDate = DateTime.now();
+
+  String get content => _content;
+
+  set content(String content) {
+    _content = content;
+    modifyDate = DateTime.now();
+  }
 
   static int _getNextId() {
     return ++_lastId;
@@ -71,7 +101,7 @@ class Todo {
 
   @override
   String toString() {
-    return 'Todo{id: $id, content: $content, createDate: $createDate, modifyDate: $modifyDate}';
+    return 'Todo{id: $id, content: $_content, createDate: $createDate, modifyDate: $modifyDate}';
   }
 }
 
@@ -80,5 +110,6 @@ void printHelp() {
   print('add - 할 일을 추가합니다.');
   print('list - 할 일 목록을 표시합니다.');
   print('del - 할 일을 삭제합니다.');
+  print('modify - 할 일을 수정합니다.');
   print('exit - 프로그램을 종료합니다.');
 }
