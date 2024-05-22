2fa48d00d217a506b45f285953589bf8e373a56b
jhs512 <jangka512@gmail.com>
2024-05-23 00:16:04 +0900

    work

--- bin/app_1.dart
+++ bin/app_1.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+
 void main() {
-  print('Hello, World!');
+  print('== 명언앱 시작 ==');
+
+  stdout.write("명령) ");
+  final String cmd = stdin.readLineSync() as String;
+
+  print('입력한 명령어 : $cmd');
+
+  print('== 명언앱 끝 ==');
 }
