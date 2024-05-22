4c9fd994318d4a814d64da0ee3089a07e8938f2b
jhs512 <jangka512@gmail.com>
2024-05-23 01:07:51 +0900

    work

--- bin/app_1.dart
+++ bin/app_1.dart
@@ -16,6 +16,26 @@ void main() {
       stdout.write('내용 : ');
       final String content = stdin.readLineSync()!.trim();
       todos.add(Todo(content));
+    } else if (cmd == 'del') {
+      stdout.write('삭제할 할 일의 번호 : ');
+
+      final id = int.tryParse(stdin.readLineSync()!.trim()) ?? -1;
+
+      if (id == -1) {
+        print('1 이상의 숫자만 입력해 주세요.');
+        continue;
+      }
+
+      final index = todos.indexWhere((todo) => todo.id == id);
+
+      if (index == -1) {
+        print('삭제할 할 일을 찾을 수 없습니다.');
+        continue;
+      }
+
+      todos.removeAt(index);
+
+      print('$id번 할 일을 삭제했습니다.');
     } else if (cmd == 'list') {
       print('번호 / 작성날짜 / 수정날짜 / 내용');
 
@@ -59,5 +79,6 @@ void printHelp() {
   print('help - 도움말을 표시합니다.');
   print('add - 할 일을 추가합니다.');
   print('list - 할 일 목록을 표시합니다.');
+  print('del - 할 일을 삭제합니다.');
   print('exit - 프로그램을 종료합니다.');
 }
